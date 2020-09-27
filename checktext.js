1258675c797f29f04ae66199349ec9392f12dc01
RackShaman <rackshaman@gmail.com>
2020-09-27 15:27:24 -0600

    firstfunction

--- checktext.js
+++ checktext.js
@@ -1,4 +1,14 @@
 function gettext()
 {
-    var btext = document.getElementByTagName("body").innertext;
+    var text = document.body.textContent;
+
+    /*var textsources = document.getElementsByTagName("body");
+    var btext = "";
+    var i;
+    for (i = 0; i < textsources.length; i++)
+    {
+        btext.concat(" ");
+        btext.concat(textsources[i]);
+    }
+    return btext;*/
 }
